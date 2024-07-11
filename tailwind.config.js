ebd9ce25641c97febf501834980032351f3e7c4a
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-07-12 01:16:26 +0600

    chore: add wrapper class, fonts and primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      jost: ['Jost', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'primary': '#F2682A'
+      }
+    },
   },
   plugins: [],
 }

3f072771af32c2974bc7aa9ae541e37ecf3cab1c
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-07-12 03:31:37 +0600

    add: reverse variant on button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,6 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-slate-900 text-slate-50 hover:bg-primary uppercase dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90",
+        reverse: "hover:bg-slate-900 text-slate-50 bg-primary uppercase dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90",
         destructive:
           "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
         outline:

727ddcd42b5a297c84f0d0cc4320353752534e7a
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-07-12 15:37:53 +0600

    setup rtk query base api

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import baseApi from "./baseApi";
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        [baseApi.reducerPath]: baseApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware)
 })
 
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;

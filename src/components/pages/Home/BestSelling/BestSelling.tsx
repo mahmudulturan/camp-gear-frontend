4c3d9d2848a976f1c26cf6e6f63b1b841adf1bb4
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-12 04:18:05 +0600

    chore: render maximum 8 product card

--- src/components/pages/Home/BestSelling/BestSelling.tsx
+++ src/components/pages/Home/BestSelling/BestSelling.tsx
@@ -16,7 +16,7 @@ const BestSelling: FC = () => {
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
                 {
-                    data?.data.map((product: IProduct) => (<ProductCard product={product} />))
+                    data?.data.slice(0, 8).map((product: IProduct) => (<ProductCard product={product} />))
                 }
             </div>
         </div>

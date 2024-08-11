8dae7dd107ea0311a2b0d6101de19734628d4cc2
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-12 04:21:19 +0600

    add: categories section  on homepage(only heading)

---
+++ src/components/pages/Home/Categories/Categories.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import SectionHeader from '../../../ui/section-header';
+
+const Categories: FC = () => {
+    return (
+        <div className='wrapper'>
+            <SectionHeader title="Shop By Category" />
+
+        </div>
+    );
+};
+
+export default Categories;
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react';
 import Banner from '../../components/pages/Home/Banner/Banner';
 import FAQSection from '../../components/pages/Home/FAQSection/FAQSection';
 import BestSelling from '../../components/pages/Home/BestSelling/BestSelling';
+import Categories from '../../components/pages/Home/Categories/Categories';
 
 const HomePage: FC = () => {
     return (
         <div className='space-y-20 mb-20'>
             <Banner />
             <BestSelling />
+            <Categories />
             <FAQSection />
         </div>
     );

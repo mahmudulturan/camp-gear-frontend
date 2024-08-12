3e7c76ecf2d85eadb448734037e158413793db56
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-12 17:33:06 +0600

    fix: make navbar as sticky

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -25,28 +25,30 @@ const Header: FC = () => {
         }
     ]
     return (
-        <div className='py-3 wrapper flex items-center justify-between'>
-            <div className='w-3/12'>
-                <Link to={'/'}>
-                    <img src={logo} alt="camp gear's logo" />
-                </Link>
-            </div>
-            <div className='md:flex items-center gap-8 hidden'>
-                {
-                    routes.map((route, index) => {
-                        return (
-                            <NavLink key={index} to={route.path} className={({ isActive }) =>
-                                isActive ? "text-primary font-medium text-[17px] uppercase duration-300" : "text-[17px] font-medium uppercase hover:text-primary duration-300"
-                            }>{route.name}</NavLink>
-                        )
-                    })
-                }
-            </div>
-            <div className='flex items-center gap-2'>
-                <button> <IoIosSearch className='size-6' /> </button>
-                <button> <HiOutlineUser className='size-6' /> </button>
-                <button> <IoMdHeartEmpty className='size-6' /> </button>
-                <button> <AiOutlineShoppingCart className='size-6' /> </button>
+        <div className='sticky top-0 bg-white/90 z-50'>
+            <div className='py-3 wrapper flex items-center justify-between overflow-hidden'>
+                <div className='w-3/12 -ml-5'>
+                    <Link to={'/'}>
+                        <img src={logo} alt="camp gear's logo" />
+                    </Link>
+                </div>
+                <div className='md:flex items-center gap-8 hidden'>
+                    {
+                        routes.map((route, index) => {
+                            return (
+                                <NavLink key={index} to={route.path} className={({ isActive }) =>
+                                    isActive ? "text-primary font-medium text-[17px] uppercase duration-300" : "text-[17px] font-medium uppercase hover:text-primary duration-300"
+                                }>{route.name}</NavLink>
+                            )
+                        })
+                    }
+                </div>
+                <div className='flex items-center gap-2'>
+                    <button> <IoIosSearch className='size-6' /> </button>
+                    <button> <HiOutlineUser className='size-6' /> </button>
+                    <button> <IoMdHeartEmpty className='size-6' /> </button>
+                    <button> <AiOutlineShoppingCart className='size-6' /> </button>
+                </div>
             </div>
         </div>
     );

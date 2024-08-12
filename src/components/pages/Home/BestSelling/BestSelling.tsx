6a6b67281a1ac57350fb4969b9cc178c554eccd7
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-12 15:44:26 +0600

    refactor: best selling section

--- src/components/pages/Home/BestSelling/BestSelling.tsx
+++ src/components/pages/Home/BestSelling/BestSelling.tsx
@@ -3,6 +3,12 @@ import SectionHeader from '../../../ui/section-header';
 import { IProduct, useGetProductsQuery } from '../../../../redux/features/productsApi';
 import ProductCard from './ProductCard';
 
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Autoplay, Pagination } from 'swiper/modules';
+import './best-selling.css';
+
 const BestSelling: FC = () => {
     const { data, isLoading } = useGetProductsQuery(undefined);
 
@@ -14,11 +20,37 @@ const BestSelling: FC = () => {
                 <SectionHeader title="Best Selling" />
                 <p className='text-textGray text-center my-4'>Don't Miss Our Best Selling</p>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={24}
+                breakpoints={{
+                    320: {
+                        slidesPerView: 1,
+                    },
+                    640: {
+                        slidesPerView: 2,
+                    },
+                    1024: {
+                        slidesPerView: 4,
+                    },
+                }}
+                className="mySwiper"
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    dynamicBullets: true,
+                    clickable: true,
+                }}
+                loop={true}
+                modules={[Autoplay, Pagination]}
+            >
                 {
-                    data?.data.slice(0, 8).map((product: IProduct) => (<ProductCard product={product} />))
+                    data?.data.slice(0, 8).map((product: IProduct) => (<SwiperSlide key={product?._id} className='mb-10'><ProductCard product={product} /></SwiperSlide>))
                 }
-            </div>
+
+            </Swiper>
         </div>
     );
 };

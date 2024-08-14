a75754223421761c6d6b9587745d7766b3c07d9b
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-14 16:56:12 +0600

    refactor: search and filter button code

--- src/pages/products/ProductsPage.tsx
+++ src/pages/products/ProductsPage.tsx
@@ -7,6 +7,7 @@ import { useGetProductsQuery } from '../../redux/features/productsApi';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '../../components/ui/select';
 import AllProducts from './components/AllProducts/AllProducts';
 import PaginationButtons from './components/PaginationButtons/PaginationButtons';
+import SearchFilter from './components/SearchFilter/SearchFilter';
 
 
 const ProductsPage: FC = () => {
@@ -14,9 +15,9 @@ const ProductsPage: FC = () => {
     const [sortValue, setSortValue] = useState<string>('');
     const { data, isLoading } = useGetProductsQuery(undefined);
 
-    
+
     const totalPages = 5;
-    
+
     if (isLoading) return <div>Loading...</div>;
 
     return (
@@ -25,31 +26,7 @@ const ProductsPage: FC = () => {
                 <SectionHeader title="All Products" />
                 <p className='text-textGray text-center my-4'>Here is a list of all products</p>
             </div>
-            <div className='flex items-center justify-between'>
-                <div className='flex-1'>
-                    <div className='relative'>
-                        <div className='absolute h-full top-0 w-9 flex items-center justify-center'>
-                            <FaSearch className='text-gray-600' />
-                        </div>
-                        <Input className='max-w-[332px] w-full pl-9 pr-3' placeholder='Search' />
-                    </div>
-
-                </div>
-                <div className='flex-1 flex items-center justify-end'>
-                    <Select onValueChange={(value) => setSortValue(value)}>
-                        <SelectTrigger className="w-[180px]">
-                            <SelectValue placeholder="Filter by Price" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectGroup>
-                                <SelectLabel>Sort by price</SelectLabel>
-                                <SelectItem value="desc">Highest</SelectItem>
-                                <SelectItem value="asc">Lowest</SelectItem>
-                            </SelectGroup>
-                        </SelectContent>
-                    </Select>
-                </div>
-            </div>
+            <SearchFilter setSortValue={setSortValue} />
             <AllProducts products={data?.data} />
             <PaginationButtons page={page} setPage={setPage} totalPages={totalPages} />
         </div>
---
+++ src/pages/products/components/SearchFilter/SearchFilter.tsx
@@ -0,0 +1,40 @@
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem } from '@radix-ui/react-select';
+import React, { FC } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { Input } from '../../../../components/ui/input';
+
+type SearchFilterProps = {
+    setSortValue: React.Dispatch<React.SetStateAction<string>>
+}
+
+const SearchFilter: FC<SearchFilterProps> = ({ setSortValue }) => {
+    return (
+        <div className='flex items-center justify-between'>
+            <div className='flex-1'>
+                <div className='relative'>
+                    <div className='absolute h-full top-0 w-9 flex items-center justify-center'>
+                        <FaSearch className='text-gray-600' />
+                    </div>
+                    <Input className='max-w-[332px] w-full pl-9 pr-3' placeholder='Search' />
+                </div>
+
+            </div>
+            <div className='flex-1 flex items-center justify-end'>
+                <Select onValueChange={(value) => setSortValue(value)}>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Filter by Price" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Sort by price</SelectLabel>
+                            <SelectItem value="desc">Highest</SelectItem>
+                            <SelectItem value="asc">Lowest</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </div>
+        </div>
+    );
+};
+
+export default SearchFilter;

3e18da2b268eaa63e4ad60e763b40e6d95abd312
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-15 17:38:17 +0600

    add: delete and update buttons on products table

--- src/pages/manage-products/components/ProductsTable/ProductsTable.tsx
+++ src/pages/manage-products/components/ProductsTable/ProductsTable.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../../../../components/ui/table';
 import { IProduct } from '../../../../redux/features/productsApi';
 import image from '../../../../assets/mock-item-images/product12_5ad78891-a8aa-4fbf-868e-91c6a471d073.webp';
+import { Button } from '../../../../components/ui/button';
+import { MdDelete, MdEdit } from 'react-icons/md';
 
 const ProductsTable: FC<{ products: IProduct[] | undefined }> = ({ products }) => {
     return (
@@ -28,7 +30,12 @@ const ProductsTable: FC<{ products: IProduct[] | undefined }> = ({ products }) =
                             <TableCell className='w-[300px]'>{product.category}</TableCell>
                             <TableCell className='w-[200px] text-center'>{product.price?.amount}</TableCell>
                             <TableCell className='w-[200px] text-center'>{product.inventory?.quantity}</TableCell>
-                            <TableCell className="text-right">{product.rating?.rate}</TableCell>
+                            <TableCell className="text-right">
+                                <div className='flex items-center gap-3 justify-end'>
+                                    <Button size={"icon"}><MdEdit className='size-6' /></Button>
+                                    <Button size={"icon"}><MdDelete className='size-6' /></Button>
+                                </div>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
